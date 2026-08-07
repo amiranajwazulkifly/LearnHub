3ef89adcb94a4cc4c4e614497cc49e1d40ff8b49
Amira Najwa <amira1004.an@gmail.com>
2026-08-08 01:00:03 +0800

    completed all frontend

--- backend/src/routes/scheduleRoutes.js
+++ backend/src/routes/scheduleRoutes.js
@@ -3,10 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 const scheduleController = require("../controllers/scheduleController");
+const validateSchedule = require("../validators/scheduleValidator");
 
 router.get("/", scheduleController.getAllSchedules);
-router.post("/", scheduleController.createSchedule);
-router.put("/:id", scheduleController.updateSchedule);
-router.delete("/:id", scheduleController.deleteSchedule);
+router.post("/", validateSchedule, scheduleController.createSchedule);
+router.put("/:id", validateSchedule, scheduleController.updateSchedule);
+router.delete("/:id", validateSchedule, scheduleController.deleteSchedule);
 
 module.exports = router;
